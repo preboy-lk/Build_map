5a2502205ee71a433657fa32f00e72f1b6ce6977
Kim Nguyen <72809118+preboy-lk@users.noreply.github.com>
2022-03-29 22:27:38 +0700

    Add files via upload

---
+++ Kim_test.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import pygame
+
+def split_animated_gif(gif_file_path):
+    ret = []
+    gif = Image.open(gif_file_path)
+    for frame_index in range(gif.n_frames):
+        gif.seek(frame_index)
+        frame_rgba = gif.convert("RGBA")
+        pygame_image = pygame.image.fromstring(
+            frame_rgba.tobytes(), frame_rgba.size, frame_rgba.mode
+        )
+        ret.append(pygame_image)
+    return ret
